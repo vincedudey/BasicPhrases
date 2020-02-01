e9a2cf68e8b53c67440d70f0c8872bae0b1ef76d
Vincent <vzhu@cpp.edu>
2020-02-01 10:53:11 -0800

    initial commit for basic phrases

---
+++ app/src/main/java/com/example/basicphrases/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.basicphrases;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void playAudio(View view){
+        Button buttonPressed = (Button)view;
+        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), getResources().getIdentifier(buttonPressed.getTag().toString(), "raw", getPackageName()));
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
